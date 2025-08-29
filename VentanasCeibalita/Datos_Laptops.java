04380ae4090cbc6c5552c5ce7afec04f748e59ef
Giani_D.S <gianina.dasilva@estudiante.ceibal.edu.uy>
2025-08-29 14:04:45 -0300

    Add files via upload

---
+++ VentanasCeibalita/Datos_Laptops.java
@@ -0,0 +1,35 @@
+package ventana2;
+
+public class Datos_Laptops {
+	
+		int id;
+		boolean prestado;
+		
+		public void Datos_Libro(int id, boolean prestado) {
+			this.id = id;
+			this. prestado = prestado;
+			
+		}
+
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+		public boolean isPrestado() {
+			return prestado;
+		}
+
+		public void setPrestado(boolean prestado) {
+			this.prestado = prestado;
+		}
+		
+		
+		
+
+	}
+
+
