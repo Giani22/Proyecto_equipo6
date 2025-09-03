aed9e1069fef47245e4a10b5aa34d8d82388feb3
Giani_D.S <gianina.dasilva@estudiante.ceibal.edu.uy>
2025-09-03 02:37:09 -0300

    Add files via upload

---
+++ PresentacionBiblotecaTodo/Ceibalita/sub/Datos_Laptops.java
@@ -0,0 +1,24 @@
+package ventana2;
+
+public class Datos_Laptops {
+	
+    int id;
+    
+    public Datos_Laptops(int id) {
+        this.id = id;
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+   
+}
+
+
+
