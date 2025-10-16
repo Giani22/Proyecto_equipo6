a88617f4f0130460e31d10aa39ebb72e756da218
Giani_D.S <gianina.dasilva@estudiante.ceibal.edu.uy>
2025-10-16 00:06:23 -0300

    Add files via upload

---
+++ Actualización16-10-25 Gian/MenuLibro.java
@@ -0,0 +1,311 @@
+package ventana;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MenuLibro extends JFrame{
+
+	Logica Gestor = new Logica();
+		public MenuLibro() {
+		       
+			 
+			
+			
+			 			//Ventana
+			 	        setTitle("Biblioteca Liceo Prof. Álvaro Figueredo");
+			 	       // setSize(1500, 1200);
+			 	        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			 	        setLocationRelativeTo(null);
+			 	        setLayout(new BorderLayout());
+			 	        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+			 	        
+			 	      
+			 	        
+			 //-------------------------------------------------------------------------------------------------------------	        
+			 	        
+			 	        
+
+			 	        // Panel Izquierdo
+			 	        JPanel panelIzquierdo = new JPanel();
+			 	        panelIzquierdo.setLayout(new BoxLayout(panelIzquierdo, BoxLayout.Y_AXIS)); 		
+			 	        panelIzquierdo.setBackground(new Color(0, 109, 109));
+			 	        panelIzquierdo.setPreferredSize(new Dimension(200, 600));
+			 	        
+			 	     //-------Boton Libro-------
+			 	       JButton btnLibro = new JButton("Libro");
+			 	       btnLibro.setFont(new Font("Arial", Font.BOLD, 15));
+			 	       btnLibro.setBackground(new Color (186, 255, 255));
+			 	       btnLibro.setAlignmentX(Component.CENTER_ALIGNMENT);
+			 	       btnLibro.setMaximumSize(new Dimension(150, 40));
+			 	       btnLibro.setForeground(Color.BLACK);
+			 	       btnLibro.setBorder(new Borde(9));
+
+			 	       panelIzquierdo.add(Box.createRigidArea(new Dimension(0, 200))); // espacio arriba
+			 	       panelIzquierdo.add(btnLibro);
+			 	       panelIzquierdo.add(Box.createVerticalGlue()); // empuja los demas hacia abajo
+
+			 	       //------Boton Salir--------
+			 	       JButton Salir = new JButton("SALIR");
+			 	       Salir.setFont(new Font("Arial", Font.BOLD, 15));
+			 	       Salir.setBackground(new Color (186, 255, 255)); 
+			 	       Salir.setAlignmentX(Component.CENTER_ALIGNMENT);
+			 	       Salir.setMaximumSize(new Dimension(150, 40));
+			 	       Salir.setForeground(Color.BLACK);
+			 	       Salir.setBorder(new Borde(9));
+			 	       
+			 	    
+
+			 	       panelIzquierdo.add(Salir);
+			 	       panelIzquierdo.add(Box.createRigidArea(new Dimension(0, 20)));
+			 	      
+			//----------------------------------------------------------------------------------------------------------
+			 	      	
+			 	       //Panel Superior
+			 	       JPanel panelSuperior = new JPanel(new FlowLayout(FlowLayout.LEFT, 20, 10));
+			 	       panelSuperior.setBackground(new Color(186, 255, 255));
+			 	        
+			 	      	
+			 	      
+			 	       //------Boton préstamo-------
+			 	        JButton btnPrestamo = new JButton("Préstamo");
+			 	        
+			 	       	btnPrestamo.setFont(new Font("Arial", Font.BOLD, 15));
+			 	      	btnPrestamo.setBackground(new Color(95, 152, 129));   // verde claro
+			 	      	btnPrestamo.setForeground(Color.BLACK);
+			 	      //	btnPrestamo.setBorder(new Borde(9));
+			 	      	
+			 	      	btnLibro.addActionListener(new ActionListener() {
+			                @Override
+			                public void actionPerformed(ActionEvent e) {
+			                    dispose(); // cierra la ventana actual
+			                  //  Gestor.ventana(); // abre la otra ventana
+			                }
+			            });
+			 	      	
+			 	      	
+			 	      	panelSuperior.add(btnPrestamo);
+			 	      	
+			 	      	//---------Boton devolución---------
+			 	      	JButton btnDevolucion = new JButton("Devolución");
+			 	      	
+			 	      	btnDevolucion.setFont(new Font("Arial", Font.BOLD, 15));
+			 	      	btnDevolucion.setBackground(new Color(95, 152, 129));
+			 	      	btnDevolucion.setForeground(Color.BLACK);
+			 	     // 	btnDevolucion.setBorder(new Borde(9));
+			 	      	
+			 	      	
+			 	      	panelSuperior.add(btnDevolucion);
+			 	      	
+			 	      	panelSuperior.add(Box.createRigidArea(new Dimension(350, 0))); // espaciado
+			 	      	panelSuperior.add(Box.createHorizontalGlue());
+			 	      
+			 	        
+			 	        
+			 	      	//-------Boton agregar-------
+			 	      	JButton btnAgregar = new JButton("+");
+			 	      	
+			 	      	btnAgregar.setFont(new Font("Arial", Font.BOLD, 18));
+			 	      	btnAgregar.setBackground(new Color(95, 152, 129));     // mismo verde del borde
+			 	      	btnAgregar.setForeground(Color.BLACK);
+			 	      //	btnAgregar.setBorder(new Borde(9));
+			 	      	
+			 	      	panelSuperior.add(btnAgregar);
+
+			 	      	
+			 	      	//-------- Boton tacho-------
+			 	      	JButton btnEliminar = new JButton("-"); 
+			 	      	
+			 	      	btnEliminar.setFont(new Font("Arial", Font.BOLD, 16));
+			 	      	btnEliminar.setBackground(new Color(95,152,129));
+			 	      	btnEliminar.setForeground(Color.BLACK);
+			 	      //	btnEliminar.setBorder(new Borde(9));
+			 	      	
+			 	        panelSuperior.add(btnEliminar);
+			 	        
+			 	       
+			 	        
+			 	       
+			 	    
+			 	        
+			 //------------------------------------------------------------------------------------------------------------
+			 	        
+			 	        
+			 	        // Panel central 
+			 	        String[] columnas = {"ISBN", "Titulo", "Autor", "Materia", "Copias", "Prestado"};
+			 	        
+			 	    // Columnas definidas
+			 	       DefaultTableModel modelo = new DefaultTableModel(columnas, 0);
+			 	        
+			 	        
+			 	       JTable tabla = new JTable(modelo);
+			 	       tabla.setGridColor(Color.BLACK);     // color de lineas
+			 	       tabla.setShowGrid(true);             // mostrar lineas
+			 	       tabla.setBackground(new Color(186, 255, 255));
+			 	       tabla.getTableHeader().setBackground(new Color(173, 226, 206));
+			 	       tabla.getTableHeader().setFont(new Font("Arial", Font.BOLD, 14));
+
+			 	       JScrollPane scrollTabla = new JScrollPane(tabla);
+			 	       scrollTabla.getViewport().setBackground(new Color(186, 255, 255));
+
+			 	       JPanel panelTabla = new JPanel(new BorderLayout());
+			 	       panelTabla.setBackground(new Color(186, 255, 255));
+			 	       panelTabla.setBorder(BorderFactory.createLineBorder(new Color(95,152,129),2));
+			 	       panelTabla.add(scrollTabla, BorderLayout.CENTER);
+			 	       
+			 	       
+			 	      String[] columnasPres = {"CI","Nombre", "Apellido", "Materia", "Grupo", "ISBN"};
+
+			 			// Columnas definidas
+			 			DefaultTableModel modeloPres = new DefaultTableModel(columnasPres, 0);
+
+			 			JTable tablaPres = new JTable(modeloPres);
+			 			tablaPres.setGridColor(Color.BLACK);     // color de lineas
+			 			tablaPres.setShowGrid(true);             // mostrar lineas
+			 			tablaPres.setBackground(new Color(186, 255, 255));
+			 			tablaPres.getTableHeader().setBackground(new Color(173, 226, 206));
+			 			tablaPres.getTableHeader().setFont(new Font("Arial", Font.BOLD, 14));
+
+			 			JScrollPane scrollTablaPres = new JScrollPane(tablaPres);
+			 			scrollTablaPres.getViewport().setBackground(new Color(186, 255, 255));
+
+			 			JPanel panelTablaPres = new JPanel(new BorderLayout());
+			 			panelTablaPres.setBackground(new Color(186, 255, 255));
+			 			panelTablaPres.setBorder(BorderFactory.createLineBorder(new Color(95,152,129),2));
+			 			panelTablaPres.add(scrollTablaPres, BorderLayout.CENTER);
+			 	    //-------------------------------------------------------------------------------------------------------------
+			 	        // Contenedor del centro (panel superior + tabla)
+			 	       
+			 	        JPanel panelCentroCompleto = new JPanel(new BorderLayout());
+			 	        panelCentroCompleto.add(panelSuperior, BorderLayout.NORTH);
+			 	        panelCentroCompleto.add(panelTabla, BorderLayout.CENTER);
+			 	        
+			 	        
+			 	   // ------------------------------------------------------------------------------------------------------------
+			 	        // Accion del boton "+"
+			 	        
+			 	       btnAgregar.addActionListener(new ActionListener() {
+			 	    	    @Override
+			 	    	    public void actionPerformed(ActionEvent e) {
+			 	    	    
+			 	    	    	Gestor.ventana4();
+			 	    	  
+			 	    	    
+			 	    	    }
+			 	    	    });
+			 	 
+			 	 // boton prestamo
+			 	       
+			 	      btnPrestamo.addActionListener(new ActionListener() {
+			 	    	    @Override
+			 	    	    public void actionPerformed(ActionEvent e) {
+			 	    	        Gestor.ventana5(); 
+			 	    	    }
+			 	    	});
+					
+			 	   // boton devolucion
+			 	       
+			 	     btnDevolucion.addActionListener(new ActionListener() {
+			 	        @Override
+			 	        public void actionPerformed(ActionEvent e) {
+			 	        
+			 	        	Gestor.ventana6();
+			 	        	
+			 	        } 
+			 	        });
+								
+								
+								
+							
+		//------------------------------------------------------------------------------------------------------------------
+			 	        
+			 	        //-----Accion del boton eliminar------
+			 	        
+			 	       btnEliminar.addActionListener(new ActionListener() {
+			 	    	    @Override
+			 	    	    public void actionPerformed(ActionEvent e) {
+			 	    	    	
+			 	    	    /*    // obtiene la fila seleccionada
+			 	    	        int filaSeleccionada = tabla.getSelectedRow();
+
+			 	    	        if (filaSeleccionada >= 0) { // por si hay fila seleccionada
+			 	    	            int confirm = JOptionPane.showConfirmDialog(
+			 	    	                    null,
+			 	    	                    "¿Deseas eliminar esta fila?",
+			 	    	                    "Confirmar eliminación",
+			 	    	                    JOptionPane.YES_NO_OPTION,
+			 	    	                    JOptionPane.WARNING_MESSAGE
+			 	    	            );
+
+			 	    	            if (confirm == JOptionPane.YES_OPTION) {
+			 	    	                modelo.removeRow(filaSeleccionada); // Elimina la fila
+			 	    	                JOptionPane.showMessageDialog(
+			 	    	                        null,
+			 	    	                        "Borrado exitosamente",
+			 	    	                        "Éxito",
+			 	    	                        JOptionPane.INFORMATION_MESSAGE
+			 	    	                );
+			 	    	            }
+			 	    	        } else { // No se seleccionó ninguna fila
+			 	    	            JOptionPane.showMessageDialog(
+			 	    	                    null,
+			 	    	                    "Por favor selecciona una fila para eliminar",
+			 	    	                    "Atención",
+			 	    	                    JOptionPane.WARNING_MESSAGE
+			 	    	            );
+			 	    	        }
+*/			 	    	    }
+			 	    	    
+			 	    	});
+
+			 	//---------------------------------------------------------------------------------------------------------     	
+			 	      
+			 	        // Panel principal
+			 	       add(panelIzquierdo, BorderLayout.WEST);   // cubre toda la altura
+			 	       add(panelCentroCompleto, BorderLayout.CENTER); // superior + tabla
+			 	       	        
+			 	        //Logo
+			 	        JLabel logo = new JLabel(new ImageIcon("logo.png"));
+			 	        logo.setBounds(0, 0, 150, 150);
+			 	        panelIzquierdo.add(logo);
+			 	        
+			 	      
+			 	        
+			 	      JButton btnListadoLib = new JButton("Listar Libros"); 
+			 	      	btnListadoLib.setFont(new Font("Arial", Font.BOLD, 15));
+			 	      	btnListadoLib.setBackground(new Color(95, 152, 129));
+			 	      	btnListadoLib.setForeground(Color.BLACK);
+			 	     
+			 	      	btnListadoLib.addActionListener(new ActionListener() {
+						@Override
+						public void actionPerformed(ActionEvent e) {
+							panelTablaPres.setVisible(false);
+							Gestor.listarLibro(modelo);
+							panelTabla.setVisible(true);
+						}
+					});
+			 	      panelSuperior.add(btnListadoLib);
+			 	    
+			 	     JButton btnListadoPres = new JButton("Listar Prestamos"); 
+			 	      	btnListadoPres.setFont(new Font("Arial", Font.BOLD, 15));
+			 	      	btnListadoPres.setBackground(new Color(95, 152, 129));
+			 	      	btnListadoPres.setForeground(Color.BLACK);
+			 	     
+			 	      	btnListadoPres.addActionListener(new ActionListener() {
+						@Override
+						public void actionPerformed(ActionEvent e) {
+							panelTabla.setVisible(false);
+							panelTablaPres.setVisible(true);
+							Gestor.listarPrestamoLib(modeloPres);
+				 	        panelCentroCompleto.add(panelTablaPres, BorderLayout.CENTER);
+						}
+					});
+			 	      panelSuperior.add(btnListadoPres);	
+		
+		
+	 	       
+			 	        
+		}	 
+}
---
+++ Actualización16-10-25 Gian/MySQL.java
@@ -0,0 +1,229 @@
+package ventana;
+
+import java.sql.*;
+
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
+
+public class MySQL {
+	
+
+	private final String DB_URL = "jdbc:mysql://23.111.185.242/program1_equipo6?useSSL=false";
+	private final String DB_USER = "program1_estudiantes";
+	private final String DB_PASS = "estudiantesarrayanes";
+	
+	private Connection getConnection() throws SQLException{
+		return DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+		
+		
+	}
+	
+	public void listarLibroBD(DefaultTableModel modelo) {
+		
+		String sentencia = "SELECT isbn,titulo,autor,materia,copias FROM libro";
+		
+		
+		try {
+			Connection conexion = getConnection();
+			PreparedStatement declaracionSQL = conexion.prepareStatement(sentencia);
+			ResultSet rs = declaracionSQL.executeQuery();
+			
+			modelo.setRowCount(0); //Borra cont tabla
+			
+			while (rs.next()) {
+				String isbn= rs.getString(1);
+				String titulo= rs.getString(2);
+				String autor = rs.getString(3);
+				String materia = rs.getString(4);
+				int copias = rs.getInt(5);
+				
+				Object[] fila = { isbn, titulo, autor, materia,copias};
+				modelo.addRow(fila);
+				
+			}
+		
+		
+		} catch(SQLException ex) {
+			JOptionPane.showMessageDialog(null, "Error: " + ex.getMessage());
+		}
+	
+	}
+	public void listarPrestamosLibBD(DefaultTableModel modeloPres) {
+		
+		String sentencia = "SELECT ci,nombre,apellido,materia,grupo,isbn FROM pideLibro";
+		
+		
+		try {
+			Connection conexion = getConnection();
+			PreparedStatement declaracionSQL = conexion.prepareStatement(sentencia);
+			ResultSet rs = declaracionSQL.executeQuery();
+			
+			modeloPres.setRowCount(0); //Borra cont tabla
+			
+			while (rs.next()) {
+				int ci = rs.getInt(1);
+				String nombre= rs.getString(2);
+				String apellido= rs.getString(3);
+				String materia = rs.getString(4);
+				String grupo = rs.getString(5);
+				String isbn = rs.getString(6);
+
+				Object[] fila = {ci, nombre, apellido, materia, grupo, isbn};
+				modeloPres.addRow(fila);
+				
+			}
+		
+		
+		} catch(SQLException ex) {
+			JOptionPane.showMessageDialog(null, "Error: " + ex.getMessage());
+		}
+	
+	}
+	public void listarCeibalitaBD(DefaultTableModel modelo) {
+		
+		String sentencia = "SELECT numero FROM Ceibalita";
+		
+		
+		try {
+			Connection conexion = getConnection();
+			PreparedStatement declaracionSQL = conexion.prepareStatement(sentencia);
+			ResultSet rs = declaracionSQL.executeQuery();
+			
+			modelo.setRowCount(0); //Borra cont tabla
+			
+			while (rs.next()) {
+			
+				int numero = rs.getInt(1);
+				
+				Object[] fila = {numero};
+				modelo.addRow(fila);
+				
+			}
+		
+		
+		} catch(SQLException ex) {
+			JOptionPane.showMessageDialog(null, "Error: " + ex.getMessage());
+		}
+	
+	}
+
+public void guardarLibroBD(Libro lib1) {
+	String sentencia = "INSERT INTO libro(isbn,titulo, autor, materia, copias) VALUES('" + lib1.getIsbn() +"','" + lib1.getTitulo() + "','" + lib1.getAutor() +"','" + lib1.getMateria()+"',"+ lib1.getCopias()+");";	
+	
+	try {
+		
+		Connection conexion = getConnection();
+		PreparedStatement declaracionSQL = conexion.prepareStatement(sentencia);
+		declaracionSQL.executeUpdate();
+		
+	} catch (SQLException ex) {
+
+		JOptionPane.showMessageDialog(null, "Error: " + ex.getMessage());
+	}
+}
+
+
+public void guardarCeibalitaBD(Datos_Laptops ceibalita) {
+	String sentencia = "INSERT INTO Ceibalita VALUES(" + ceibalita.getNumero()+");";	
+	try {
+		
+		Connection conexion = getConnection();
+		PreparedStatement declaracionSQL = conexion.prepareStatement(sentencia);
+		declaracionSQL.executeUpdate();
+		
+	} catch (SQLException ex) {
+
+		JOptionPane.showMessageDialog(null, "Error: " + ex.getMessage());
+	}
+}
+
+	public void guardarPrestamoLibBD(Solicitante soli) {
+	String sentencia = "INSERT INTO pideLibro(ci, nombre, apellido, materia, grupo, isbn) VALUES('" + soli.getCi() + "','"  + soli.getNombre() + "','" + soli.getApellido() + "','" + soli.getMateria()+"','" + soli.getGrupo()+"','" + soli.getIsbn() + "');";
+	String sentencia2 = "INSERT INTO personas(ci, nombre, apellido, materia, grupo) VALUES('" + soli.getCi() +"','" + soli.getNombre() + "','" + soli.getApellido() + "','"  + soli.getMateria()+ "','" + soli.getGrupo()+ "');";
+
+	
+	System.out.println(sentencia);
+	System.out.println(sentencia2);
+	try {
+		
+		Connection conexion = getConnection();
+		PreparedStatement declaracionSQL = conexion.prepareStatement(sentencia);
+		PreparedStatement declaracionSQL2 = conexion.prepareStatement(sentencia2);
+
+		declaracionSQL.executeUpdate();
+		declaracionSQL2.executeUpdate();
+
+		
+	} catch (SQLException ex) {
+
+		JOptionPane.showMessageDialog(null, "Error: " + ex.getMessage());
+	}
+	}
+
+	public void guardarPrestamoCeibBD(Solicitante2 soli) {
+		
+		String sentencia = "INSERT INTO pideCeibalita(ci ,nombre, apellido, materia, grupo, numero) VALUES('" + soli.getCi() +"','"+ soli.getNombre() + "','" + soli.getApellido() + "','" + soli.getMateria()+"','" + soli.getGrupo()+"','" + soli.getNumero() + "');";
+		String sentencia2 = "INSERT INTO personas(ci, nombre, apellido, materia, grupo) VALUES('" + soli.getCi() +"','"+ soli.getNombre() + "','" + soli.getApellido() + "','" + soli.getMateria()+"','" + soli.getGrupo()+ "');";
+		System.out.println(sentencia);
+		System.out.println(sentencia2);
+			try {
+				
+				Connection conexion = getConnection();
+				PreparedStatement declaracionSQL = conexion.prepareStatement(sentencia);
+				PreparedStatement declaracionSQL2 = conexion.prepareStatement(sentencia2);
+				declaracionSQL.executeUpdate();
+				declaracionSQL2.executeUpdate();
+				
+			} catch (SQLException ex) {
+
+				JOptionPane.showMessageDialog(null, "Error: " + ex.getMessage());
+			}
+			}
+		
+	public void listarPrestamosCeibBD(DefaultTableModel modelo) {
+		
+			String sentencia = "SELECT ci,nombre,apellido,materia,grupo,numero FROM pideCeibalita";
+			
+		
+		
+			try {
+				Connection conexion = getConnection();
+				PreparedStatement declaracionSQL = conexion.prepareStatement(sentencia);
+				ResultSet rs = declaracionSQL.executeQuery();
+			
+				modelo.setRowCount(0); //Borra cont tabla
+			
+				while (rs.next()) {
+					int ci = rs.getInt(1);
+					String nombre= rs.getString(2);
+					String apellido= rs.getString(3);
+					String materia = rs.getString(4);
+					String grupo = rs.getString(5);
+					int numero = rs.getInt(6);
+
+				Object[] fila = { ci,nombre, apellido, materia, grupo, numero};
+					modelo.addRow(fila);
+				
+				}
+		
+		
+			} catch(SQLException ex) {
+			JOptionPane.showMessageDialog(null, "Error: " + ex.getMessage());
+			}
+	}
+	public void elimLibroBD(Libro lib1) {
+		
+		String sentencia = "DELETE From libros Where ci = " + lib1.getIsbn();
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+}
+		
+			
